d7b7457e34ef75dd88c43e5312168c3a4568899d
azeezabidoye <azeezabidoye@yahoo.com>
2025-04-17 12:20:16 +0100

    Deploy Script Updated

--- deploy/deploy.cjs
+++ deploy/deploy.cjs
@@ -2,6 +2,9 @@ const hre = require("hardhat");
 
 async function main() {
   const Tracking = await hre.ethers.getContractFactory("Tracking");
+  const tracking = await Tracking.deploy();
+
+  await tracking.deployed();
 }
 
 main.catch((error) => {
